f99cd98c28b9d91905225c02c211e6720566bf69
Tirapol Jansiricharoen <tjansirichar@Tirapols-MacBook-Air.local>
2024-06-15 00:21:09 +0700

    Add: edit book feature

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -92,6 +93,52 @@ func (h *Handler) AddNewBook(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/api/books", http.StatusSeeOther)
 }
 
-func (h *Handler) UpdateBook(w http.ResponseWriter, r *http.Request) {}
+func (h *Handler) EditBookForm(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	bookIDstr := mux.Vars(r)["bookID"]
+	bookID, err := strconv.ParseUint(bookIDstr, 10, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid ID"))
+		return
+	}
+
+	db := h.BookDB.GetDB()
+	book, err := model.GetBookByID(db, uint(bookID))
+	if err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("No Book with this ID"))
+		return
+	}
+
+	err = h.Template.ExecuteTemplate(w, "edit.html", book)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+}
+
+func (h *Handler) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	bookIDstr := mux.Vars(r)["bookID"]
+	bookID, err := strconv.ParseUint(bookIDstr, 10, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid ID"))
+		return
+	}
+
+	bookData, _ := ioutil.ReadAll(r.Body)
+	db := h.BookDB.GetDB()
+	err = model.UpdateBook(db, uint(bookID), bookData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+}
 
 func (h *Handler) DeleteBook(w http.ResponseWriter, r *http.Request) {}
